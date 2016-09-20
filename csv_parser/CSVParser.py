e61f8d18c2f0a6ad2d89291ee700e8f3b908d196
Alron <alronjan@yahoo.com>
2016-09-20 19:13:24 +0800

    Partially implemented Driver code for testing Senti data. Only file reading is implemented as of this version.

--- csv_parser/CSVParser.py
+++ csv_parser/CSVParser.py
@@ -1,11 +1,32 @@
+from itertools import chain
 import csv
+import os
 class CSVParser:
 
     @staticmethod
-    def parse_file_into_csv_row_generator(file_name):
-     with open('../'+file_name, newline='') as csv_file:
-         row_reader = csv.reader(csv_file, delimiter=',')
-         for row in row_reader:
-            yield_row = [x for x in row if x.strip() != "" ]
-            if yield_row.__len__() > 0:
-                yield yield_row
+    def parse_file_into_csv_row_generator(file):
+        with open(file.absolute().__str__(), newline='') as csv_file:
+            row_reader = csv.reader(csv_file, delimiter=',')
+
+            for row in CSVParser.iterator_wrapper(row_reader):
+                yield_row = [x for x in row if x.strip() != "" ]
+                if yield_row.__len__() > 0:
+                    yield yield_row
+
+    @staticmethod
+    def iterator_wrapper(generator):
+        while True:
+            try:
+              yield next(generator)
+            except StopIteration:
+              raise
+            except Exception as e:
+              print(e) # or whatever kind of logging you want
+              pass
+
+    @staticmethod
+    def parse_files_into_csv_row_generator(files):
+        generator = iter(())
+        for file in files:
+            generator = chain(generator, CSVParser.parse_file_into_csv_row_generator(file))
+        return generator
--- sentiment_analysis/Driver.py
+++ sentiment_analysis/Driver.py
@@ -1,6 +1,8 @@
 import nltk
 import math
 import random
+from foldersio import FolderIO
+from csv_parser import CSVParser
 from csv_parser.CSVParser import CSVParser
 from nltk.classify.naivebayes import NaiveBayesClassifier
 from sentiment_analysis.feature_extraction.UnigramExtractor import UnigramExtractor
@@ -19,43 +21,57 @@ def train_or_load(pickle_file_name, trainer, training_set, force_train=False):
         save_classifier_to_pickle(pickle_file_name, classifier)
     return classifier
 
-# read data
-LIMIT = 213
+def test_nltk():
+    # read data
+    LIMIT = 213
 
-row_generator = CSVParser.parse_file_into_csv_row_generator('sa_training_data/globe_dataset.csv')
-tweet_texts = [row[2] for row in row_generator][:LIMIT]
+    row_generator = CSVParser.parse_file_into_csv_row_generator('sa_training_data/globe_dataset.csv')
+    tweet_texts = [row[2] for row in row_generator][:LIMIT]
 
-row_generator = CSVParser.parse_file_into_csv_row_generator('sa_training_data/globe_dataset.csv')
-tweet_labels = [row[1] for row in row_generator][:LIMIT]
+    row_generator = CSVParser.parse_file_into_csv_row_generator('sa_training_data/globe_dataset.csv')
+    tweet_labels = [row[1] for row in row_generator][:LIMIT]
 
-# pre-process tweets
-TWEET_PREPROCESSORS = [SplitWordByWhitespace(), WordLengthFilter(3), WordToLowercase()]
-tweet_texts = preprocess_tweets(tweet_texts, TWEET_PREPROCESSORS)
+    # pre-process tweets
+    TWEET_PREPROCESSORS = [SplitWordByWhitespace(), WordLengthFilter(3), WordToLowercase()]
+    tweet_texts = preprocess_tweets(tweet_texts, TWEET_PREPROCESSORS)
 
-# construct labeled tweets to be run with the classifiers
-labeled_tweets = list(zip(tweet_texts, tweet_labels))
+    # construct labeled tweets to be run with the classifiers
+    labeled_tweets = list(zip(tweet_texts, tweet_labels))
 
 
-# partition training/testing sets
-random.shuffle(labeled_tweets) # shuffling here to randomize train and test tweets
-num_train = math.floor(LIMIT * 0.6)
-train_tweets = labeled_tweets[:num_train]
-test_tweets = labeled_tweets[num_train:]
+    # partition training/testing sets
+    random.shuffle(labeled_tweets) # shuffling here to randomize train and test tweets
+    num_train = math.floor(LIMIT * 0.6)
+    train_tweets = labeled_tweets[:num_train]
+    test_tweets = labeled_tweets[num_train:]
 
-print("# TRAIN: {}".format(train_tweets.__len__()))
-print("# TEST: {}".format(test_tweets.__len__()))
+    print("# TRAIN: {}".format(train_tweets.__len__()))
+    print("# TEST: {}".format(test_tweets.__len__()))
 
-# feature extraction
-FEATURE_EXTRACTOR = UnigramExtractor(train_tweets)
-FeatureExtractorBase.save_feature_extractor("unigram_feature_extractor.pickle", FEATURE_EXTRACTOR)
-training_set = nltk.classify.apply_features(FEATURE_EXTRACTOR.extract_features, train_tweets)
+    # feature extraction
+    FEATURE_EXTRACTOR = UnigramExtractor(train_tweets)
+    FeatureExtractorBase.save_feature_extractor("unigram_feature_extractor.pickle", FEATURE_EXTRACTOR)
+    training_set = nltk.classify.apply_features(FEATURE_EXTRACTOR.extract_features, train_tweets)
 
-# training
-TRAINER = nltk.NaiveBayesClassifier
-# TRAINER = SklearnClassifier(BernoulliNB())
-classifier = train_or_load("nb_classifier.pickle", TRAINER, training_set, True)
-print(classifier.show_most_informative_features(15))
+    # training
+    TRAINER = nltk.NaiveBayesClassifier
+    # TRAINER = SklearnClassifier(BernoulliNB())
+    classifier = train_or_load("nb_classifier.pickle", TRAINER, training_set, True)
+    print(classifier.show_most_informative_features(15))
 
-#classification
-test_set = nltk.classify.apply_features(FEATURE_EXTRACTOR.extract_features, test_tweets)
-print(nltk.classify.accuracy(classifier, test_set))
+    #classification
+    test_set = nltk.classify.apply_features(FEATURE_EXTRACTOR.extract_features, test_tweets)
+    print(nltk.classify.accuracy(classifier, test_set))
+
+
+def test_senti_election_data():
+    print("INSIDE HERE")
+    csv_files = FolderIO.get_files('D:/DLSU/Masters/MS Thesis/data-2016/senti_election_data/csv_files', False, '.csv')
+    print("FINISHED READING CSV FILES")
+    print(csv_files.__len__())
+    csv_rows = CSVParser.parse_files_into_csv_row_generator(csv_files)
+    print("FINISHED READIN CSV ROWS")
+    for csv_row in csv_rows:
+        print(csv_row)
+test_senti_election_data()
+
