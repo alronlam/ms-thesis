6fc1afc1d6ea5e59a21c771dc16a7e20fad47fc4
Alron <alronjan@yahoo.com>
2017-01-24 16:14:07 +0800

    Added code for generating csv files from the original txt files.

---
+++ pcari/CSVGeneratorDriver.py
@@ -0,0 +1,22 @@
+import csv
+import os
+
+from twitter_data.parsing.folders import FolderIO
+
+data_source_dir ="C:/Users/user/PycharmProjects/ms-thesis/pcari/data/Yolanda Tweets - Nov 2013 - Feb 2014"
+
+txt_files = FolderIO.get_files(data_source_dir, False, ".txt")
+
+dataset_arr = []
+
+for txt_file in txt_files:
+    label = os.path.splitext(txt_file.name)[0]
+    with txt_file.open(encoding="utf8") as input_file:
+        tweet_texts = [line.rstrip() for line in input_file.readlines() if line.rstrip() is not None]
+        dataset_arr.extend([(tweet_text, label)for tweet_text in tweet_texts])
+
+
+with open("yolanda_nov2013_feb2014_dataset.csv", "w", encoding="utf8", newline='') as output_file:
+    csv_writer = csv.writer(output_file)
+    for tuple in dataset_arr:
+        csv_writer.writerow(tuple)
