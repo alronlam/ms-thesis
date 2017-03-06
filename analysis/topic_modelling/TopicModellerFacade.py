774238f8f88f45056e92523c89ae1a269e6cecc4
Alron <alronjan@yahoo.com>
2017-03-07 01:46:29 +0800

    Added text pre-processing option to topic modelling.

--- analysis/topic_modelling/TopicModellerFacade.py
+++ analysis/topic_modelling/TopicModellerFacade.py
@@ -1,8 +1,9 @@
 from community_detection.Utils import get_communities, get_vertex_ids_in_each_community, \
     filter_tweets_belonging_to_user_ids, get_user_ids_from_vertex_ids
+from sentiment_analysis.preprocessing import PreProcessing
 
 
-def construct_topic_models_for_communities(topic_modeller, graph, membership, tweet_objects):
+def construct_topic_models_for_communities(topic_modeller, graph, membership, tweet_objects, preprocessors=[]):
 
     community_topics_tuple_list = []
 
@@ -11,7 +12,7 @@ def construct_topic_models_for_communities(topic_modeller, graph, membership, tw
     for community_num, vertex_ids in enumerate(vertex_ids_per_community):
         user_ids_str = get_user_ids_from_vertex_ids(graph, vertex_ids)
         tweet_texts = filter_tweets_belonging_to_user_ids(tweet_objects, user_ids_str)
-
+        tweet_texts = PreProcessing.preprocess_tweets(tweet_texts, preprocessors)
         community_topics_tuple_list.append((community_num, topic_modeller.model_topics(tweet_texts)))
 
     return community_topics_tuple_list
