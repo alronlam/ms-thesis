5455e6f75e4788c8249ca30a0681828094bf0c56
Alron <alronjan@yahoo.com>
2017-03-11 15:56:45 +0800

    Added function for retrieving tweets in conversational context. Untested.

--- twitter_data/database/DBUtils.py
+++ twitter_data/database/DBUtils.py
@@ -12,3 +12,14 @@ def retrieve_all_tweet_objects_from_db(tweet_ids, verbose=False):
                 print("Processed {}. Retrieved {} ({} successful) / {} tweets from the database.".format(tweet_id,index+1, len(tweet_objects), len(tweet_ids)))
 
     return tweet_objects
+
+def retrieve_conversational_tweets(tweet_id, curr_list=[]):
+    tweet = DBManager.get_or_add_tweet(tweet_id)
+    contextual_tweet = DBManager.get_or_add_tweet(tweet.in_reply_to_status_id_str)
+
+    if not contextual_tweet:
+        return curr_list
+
+    curr_list.append(contextual_tweet)
+    return retrieve_conversational_tweets(tweet.in_reply_to_status_id_str, curr_list)
+
