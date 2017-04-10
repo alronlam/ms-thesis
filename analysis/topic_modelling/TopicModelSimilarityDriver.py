0a8162da3c63ebdafeddc1abbcaa36115fcbf6c5
Alron <alronjan@yahoo.com>
2017-04-10 18:18:55 +0800

    Added new functionality and some bug fixes.

--- analysis/topic_modelling/TopicModelSimilarityDriver.py
+++ analysis/topic_modelling/TopicModelSimilarityDriver.py
@@ -72,32 +72,54 @@ def generate_avg_topic_pairwise_similarity_score(topic_model1, topic_model2):
     if len(topic_model1) == 0 or len(topic_model2) == 0:
         return 0
 
-    total = 0
+    topic_model1 = normalize_topic_weights(topic_model1)
+    topic_model2 = normalize_topic_weights(topic_model2)
+
+    scores_weights = []
 
     for word_topic1, weight_topic1 in topic_model1:
         for word_topic2, weight_topic2 in topic_model2:
-            similarity_score = get_or_add_coherence_score(word_topic1, word_topic2, coherence_type=COHERENCE_TYPE) * weight_topic1 * weight_topic2
+            similarity_score = get_or_add_coherence_score(word_topic1, word_topic2, coherence_type=COHERENCE_TYPE) #* weight_topic1 * weight_topic2
             # print("{} , {} = {}".format(word_topic1, word_topic2, similarity_score))
-            total += similarity_score
-    return total / (len(topic_model1)* len(topic_model2))
+            scores_weights.append((similarity_score, weight_topic1 * weight_topic2) )
+
+    total_score_weights = sum([weight for score, weight in scores_weights])
+    final_score = sum([score * weight/total_score_weights for score, weight in scores_weights])
+
+    return final_score
+
+def generate_count_topic_pairwise_similarity_overlap(topic_model1, topic_model2):
+    if len(topic_model1) == 0 or len(topic_model2) == 0:
+        return 0
+
+    total = 0
+
+    words_topic1 = [word for word, weight in topic_model1]
+    words_topic2 = [word for word, weight in topic_model2]
+
+    word_set1 = set(words_topic1)
+    word_set2 = set(words_topic2)
+
+    intersection = word_set1.intersection(word_set2)
+    union = word_set1.union(word_set2)
+
+    # return len(intersection) / len(union)
+    return len(intersection)
 
 def normalize_topic_weights(topic_models):
     if topic_models:
-        max_weight = max([weight for word, weight in topic_models])
-        return [(word, weight/max_weight) for word, weight in topic_models ]
+        total_weights = sum([weight for word, weight in topic_models])
+        return [(word, weight/total_weights) for word, weight in topic_models ]
 
 def generate_community_pairwise_similarity_matrix(community_models1, community_models2):
 
     if len(community_models1) == 0 or len(community_models2) == 0:
         return []
 
-
     matrix = [[0 for x in range(len(community_models1))] for x in range(len(community_models2))]
 
     for topic_num1, topic_model1 in community_models1:
-        topic_model1 = normalize_topic_weights(topic_model1)
         for topic_num2, topic_model2 in community_models2:
-            topic_model2 = normalize_topic_weights(topic_model2)
             print("Generating Topic {}-{}".format(topic_num1, topic_num2))
             matrix[topic_num1][topic_num2] = generate_avg_topic_pairwise_similarity_score(topic_model1, topic_model2)
 
@@ -152,7 +174,91 @@ def save_topic_similarities(topic_similarity_matrices, output_dir, graph_scheme)
     #         csv_writer.writerows(topic_similarity_matrix)
 
 
-# print(calculate_similarity_score(["apple","banana"]))
+def generate_string_for_topics(topics):
+    topic_string = []
+    for topic_num, word_weight_tuples in topics:
+            topic_string.append("\nTopic {}".format(topic_num))
+            topic_string.extend(["{} - {}".format(word, weight) for word, weight in word_weight_tuples])
+    return "\n".join(topic_string)
+
+
+def save_topic_models_to_readable_format_if_not_exists(community_topic_models, file_path):
+    try:
+        open(file_path, "r", encoding="utf-8")
+    except Exception as e:
+        topic_models_file = open(file_path, "w", encoding="utf-8")
+        for index, community_topic_model in enumerate(community_topic_models):
+            print("Community {}:\n{}\n\n".format(index, generate_string_for_topics(community_topic_model)), file=topic_models_file)
+        topic_models_file.close()
+
+def clean_topic_models(community_topic_models):
+    def remove_ellipsis(word):
+        return re.sub("…", "", word)
+
+    for index, community_topic_model in enumerate(community_topic_models):
+        for topic_num, word_weight_tuples in community_topic_model:
+            cleaned = [(remove_ellipsis(word), weight) for word, weight in word_weight_tuples]
+            community_topic_model[topic_num] = (topic_num, cleaned)
+    return community_topic_models
+
+def count_match(similarity_matrix, threshold=0):
+    count = 0
+    for row in similarity_matrix:
+        for col in row:
+            if col > threshold:
+                count += 1
+    return count
+
+def remove_headers_from_similarity_matrix(similarity_matrix):
+    similarity_matrix = similarity_matrix[1:]
+    similarity_matrix = [row[1:] for row in similarity_matrix]
+    return similarity_matrix
+
+def generate_summary_matrix(similarity_matrices):
+    summary_matrix = [[0 for x in range(6)] for x in range(6) ]
+
+    for index1, index2, similarity_matrix in similarity_matrices:
+        similarity_matrix = remove_headers_from_similarity_matrix(similarity_matrix)
+        summary_matrix[index1][index2] = count_match(similarity_matrix, 0.1)
+
+    return summary_matrix
+
+def common_word_string(similarity_matrix, threshold=0):
+    count = 0
+    common_words = set()
+    for row_index in range(1, len(similarity_matrix)):
+        row = similarity_matrix[row_index]
+        for col_index in range(1, len(row)):
+            col = row[col_index]
+            if col > threshold:
+                count += 1
+                word_set1 = set(similarity_matrix[row_index][0].split())
+                word_set2 = set(similarity_matrix[0][col_index].split())
+                common_words = common_words.union(word_set1.intersection(word_set2))
+                # common_words = common_words.union(word_set1)
+                # common_words = common_words.union(word_set2)
+
+
+    common_words_string = " ".join(list(common_words)) if len(common_words) > 0 else "None"
+    return common_words_string, count
+
+def generate_summary_common_word_matrix(similarity_matrices):
+    word_matrix = [["None" for x in range(6)] for x in range(6) ]
+    count_matrix = [[0 for x in range(6)] for x in range(6) ]
+
+    for index1, index2, similarity_matrix in similarity_matrices:
+        word_matrix[index1][index2], count_matrix[index1][index2] = common_word_string(similarity_matrix, 0)
+
+    return word_matrix, count_matrix
+
+def get_words_from_topic(word_weight_tuples):
+    return " ".join([word for word, weight in word_weight_tuples])
+
+def flatten_to_1d(similarity_matrix):
+    flattened = []
+    for row in similarity_matrix[1:]:
+        flattened.extend(row[1:])
+    return flattened
 
 root_folder="graphs"
 dirs = [
@@ -160,7 +266,8 @@ dirs = [
         "contextualsa",
         "sa",
         "hashtags",
-        "scoring"
+        "scoring-sa",
+        "scoring-contextualsa"
         ]
 
 # #generate topic models
@@ -179,13 +286,53 @@ for dir in dirs:
         community_topic_models = [generate_topic_model(community_doc) for community_doc in community_docs]
         pickle.dump(community_topic_models, open(root_folder+"/"+dir+"/"+dir+"-topic_models.pickle", "wb"))
 
+    save_topic_models_to_readable_format_if_not_exists(community_topic_models,"{}/{}/{}-topic-models-cleaned.txt".format(root_folder, dir, dir) )
     print("Generating topic similarities")
     topic_similarities = generate_topic_similarities(community_topic_models, root_folder+"/"+dir)
     save_topic_similarities(topic_similarities, root_folder+"/"+dir, dir)
 
-# generate topic similarity scores
-# for dir in dirs:
-#     community_topic_models = pickle.load(open(root_folder+"/"+dir+"/"+dir+"-topic_models.pickle", "rb"))
-#     # print(community_topic_models[0])
-#     topic_similarities = generate_topic_similarities(community_topic_models, root_folder+"/"+dir)
-#     save_topic_similarities(topic_similarities, root_folder+"/"+dir, dir)
+
+# Driver code to insert missing header in saved similarity matrices
+for dir in dirs:
+    print("Loading files")
+    similarity_matrices = pickle.load(open("{}/{}/{}_similarity_matrices.pickle".format(root_folder, dir, dir), "rb"))
+    print("Loading topic models")
+    community_topic_models = pickle.load(open(root_folder+"/"+dir+"/"+dir+"-topic_models.pickle", "rb"))
+
+    topic_model_words = [[""]+[get_words_from_topic(topic) for topic_num, topic in topic_model] for topic_model in community_topic_models]
+    topic_model_words = [word_list for word_list in topic_model_words if len(word_list) > 1]
+    indices = [0,1,2,3,1,2,3,2,3,3]
+    print(topic_model_words)
+    for matrix_index, (index1, index2, similarity_matrix) in enumerate(similarity_matrices):
+        similarity_matrix.insert(0, topic_model_words[indices[matrix_index]])
+        # print(topic_model_words[indices[matrix_index]])
+    pickle.dump(similarity_matrices, open("{}/{}/{}_similarity_matrices_new.pickle".format(root_folder, dir, dir), "wb"))
+
+
+# generate summary matrices
+for dir in dirs:
+    similarity_matrices = pickle.load(open("{}/{}/{}_similarity_matrices_new.pickle".format(root_folder, dir, dir), "rb"))
+    # summary_matrix = generate_summary_matrix(similarity_matrices)
+    # pickle.dump(summary_matrix, open("{}/{}/{}_summary_matrix.pickle".format(root_folder, dir, dir), "wb"))
+    # with open("{}/{}/{}_summary_matrix.csv".format(root_folder, dir, dir), "w", encoding="utf-8", newline='') as csv_file:
+    #     csv_writer = csv.writer(csv_file)
+    #     csv_writer.writerows(summary_matrix)
+
+    flattened_similarity_matrices = [flatten_to_1d(similarity_matrix) for index1, index2, similarity_matrix in similarity_matrices]
+    with open("{}/{}/{}_flattened_similarity_matrices_npmi2.csv".format(root_folder, dir, dir), "w", encoding="utf-8", newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerows(flattened_similarity_matrices)
+
+    # word_matrix, count_matrix = generate_summary_common_word_matrix(similarity_matrices)
+    # pickle.dump(word_matrix, open("{}/{}/{}_word_matrix.pickle".format(root_folder, dir, dir), "wb"))
+    # with open("{}/{}/{}_word_matrix.csv".format(root_folder, dir, dir), "w", encoding="utf-8", newline='') as csv_file:
+    #     csv_writer = csv.writer(csv_file)
+    #     csv_writer.writerows(word_matrix)
+    #
+    # pickle.dump(count_matrix, open("{}/{}/{}_count_matrix.pickle".format(root_folder, dir, dir), "wb"))
+    # with open("{}/{}/{}_count_matrix.csv".format(root_folder, dir, dir), "w", encoding="utf-8", newline='') as csv_file:
+    #     csv_writer = csv.writer(csv_file)
+    #     csv_writer.writerows(count_matrix)
+
+
+
