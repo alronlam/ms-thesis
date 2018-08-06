e4b52d00ba562a59572aed9b1db8f7d8b31583c8
Alron <alronjan@yahoo.com>
2018-08-06 18:06:22 +0800

    Update topic sample tweets driver Updated to also output the tweet that has highest probability of belonging to each topic.

--- analysis/topic_modelling/LDATopicModeller.py
+++ analysis/topic_modelling/LDATopicModeller.py
@@ -45,7 +45,8 @@ class LDATopicModeller(TopicModeller):
         document_list_cleaned = [self.preprocess_document(document) for document in document_list]
         dictionary = corpora.Dictionary(document_list_cleaned)
         doc_term_matrix = [dictionary.doc2bow(document) for document in document_list_cleaned]
-        lda_model = self.LDA(doc_term_matrix, num_topics=self.num_topics, id2word=dictionary, passes=50, iterations=100, eval_every=1)
+        lda_model = self.LDA(doc_term_matrix, num_topics=self.num_topics, id2word=dictionary, passes=50, iterations=100,
+                             eval_every=1)
         word_weight_tuples = lda_model.show_topics(num_topics=self.num_topics, num_words=self.num_words, log=False,
                                                    formatted=False)
 
@@ -53,7 +54,7 @@ class LDATopicModeller(TopicModeller):
         self.dictionary = dictionary
         self.word_weight_tuples = word_weight_tuples
 
-    def closest_topic(self, document):
+    def closest_topic(self, document, with_score=False):
         document = self.preprocess_document(document)
         bow = self.dictionary.doc2bow(document)
         topic_probabilities = self.lda_model.get_document_topics(bow)
@@ -61,7 +62,11 @@ class LDATopicModeller(TopicModeller):
         topics = [topic for (topic, probability) in topic_probabilities]
         probabilities = numpy.array([probability for (topic, probability) in topic_probabilities])
         max_index = probabilities.argmax()
-        return topics[max_index]
+
+        if with_score:
+            return topics[max_index], probabilities[max_index]
+        else:
+            return topics[max_index]
 
     def preprocess_document(self, document):
         stop_free = " ".join([i for i in document.lower().split() if i not in self.stop_words])
--- dsaa/topic_sample_tweets_driver.py
+++ dsaa/topic_sample_tweets_driver.py
@@ -4,7 +4,7 @@ import pickle
 from collections import Counter
 
 import settings
-from dsaa import text_loader, utils
+from dsaa import text_loader
 from sentiment_analysis.preprocessing import PreProcessing
 
 OUTPUT_DIR = '{}/dsaa_results/topic_models'.format(settings.PROJECT_ROOT)
@@ -27,42 +27,51 @@ preprocessors = [PreProcessing.SplitWordByWhitespace(),
                                                 ignore_case=True),
                  PreProcessing.ConcatWordArray()]
 
+
 if __name__ == "__main__":
-
-    utils.create_path_if_not_exists(OUTPUT_DIR)
-
-    log_file_path = "{}/topic_models_raw_log.txt".format(OUTPUT_DIR)
 
     lda_models = pickle.load(open("{}/scheme_lda_models.pickle".format(OUTPUT_DIR), "rb"))
 
-    with open(log_file_path, "w") as log_file:
-        for config in text_loader.CONFIGS:
-            print("### {} ###".format(config), file=log_file)
-            log_file.flush()
-            dir = os.path.join(text_loader.TEXTS_ROOT_DIR, config)
+    for config in text_loader.CONFIGS:
+        print("\n\n### {} ###".format(config))
+        dir = os.path.join(text_loader.TEXTS_ROOT_DIR, config)
 
-            community_docs = text_loader.load_community_docs(dir)
-            original_community_docs = copy.deepcopy(community_docs)
-            community_docs = [PreProcessing.preprocess_strings(x, preprocessors)
-                              for x in community_docs]
+        original_community_docs = text_loader.load_community_docs(dir)
+        community_docs = copy.deepcopy(original_community_docs)
+        community_docs = [PreProcessing.preprocess_strings(x, preprocessors)
+                          for x in community_docs]
 
-            closest_topic_map = {}
-            lda = lda_models[config]
+        topic_tweets_map = {}
+        max_similarity_map = {}
 
-            for community_num, (docs, original_docs) in enumerate(zip(community_docs, original_community_docs)):
+        lda = lda_models[config]
 
-                print("\n***\nCommunity {}".format(community_num))
+        for community_num, (docs, original_docs) in enumerate(zip(community_docs, original_community_docs)):
 
-                for doc, original in zip(docs, original_docs):
-                    closest_topic = lda.closest_topic(doc)
-                    curr_list = closest_topic_map.get(closest_topic, [])
-                    closest_topic_map[closest_topic] = curr_list + [original]
+            # print("\n***\nCommunity {}".format(community_num))
 
-                for topic_num, docs in closest_topic_map.items():
-                    print("\nTopic {}".format(topic_num))
-                    counter = Counter(docs)
-                    most_common = counter.most_common(3)
-                    most_common_strs = ["({}) {}".format(x[1], x[0])
-                                        for x in most_common]
-                    collated_str = "\n".join(most_common_strs)
-                    print(collated_str)
+            for doc, original in zip(docs, original_docs):
+
+                # Build map for most common tweet for each topic
+                closest_topic, score = lda.closest_topic(doc, with_score=True)
+                curr_list = topic_tweets_map.get(closest_topic, [])
+                topic_tweets_map[closest_topic] = curr_list + [original]
+
+                # Build map for most "representative" tweet for each topic
+                curr_max_tweet, curr_max_score =max_similarity_map.get(closest_topic, ("", -100))
+                if score > curr_max_score:
+                    max_similarity_map[closest_topic] = (original, score)
+
+        for topic_num, docs in topic_tweets_map.items():
+            print("\n------------\nTopic {}".format(topic_num))
+            n = 1
+            print("> Most Frequent {} Tweet(s):".format(n))
+            counter = Counter(docs)
+            most_common = counter.most_common(n)
+            most_common_strs = ["{}".format(x[0])
+                                for x in most_common]
+            collated_str = "\n".join(most_common_strs)
+            print(collated_str)
+
+            print("\n> Most Representative Tweet:")
+            print("{}".format(max_similarity_map[topic_num][0]))
